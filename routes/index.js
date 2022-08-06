9f3109910dd9ba0c53f51e7384b8276069f895ec
swaroopa1986 <54567036+swaroopa1986@users.noreply.github.com>
2022-08-06 12:49:53 +0100

    changed index.js and app.js

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.set('X-Frame-Options', 'DENY')
+  res.set('X-Content-Type-Options', 'nosniff')
+  res.set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload')
+  res.set('Cache-Control', 'no-cache, no-store, must-revalidate')
+  res.set('Content-Security-Policy', "default-src 'none'; script-src 'self'; connect-src 'self'; img-src 'self'; style-src 'self'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'")
   res.render('index', { title: "Simple Application" });
 });
 
